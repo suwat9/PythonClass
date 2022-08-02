d977a21f0c91982eafcbe6eb16dc0c54196b4c97
Suwat <suwat.tac@mail.pbru.ac.th>
2022-08-02 10:51:48 +0700

    Update testNumber.py

--- Codes/testNumber.py
+++ Codes/testNumber.py
@@ -38,5 +38,6 @@ daywk = studyTime.weekday()
 monthstr = ['','มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม',
             'กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม']
 monthNo = studyTime.month
+yearthai = studyTime.year + 543
 
-print(dayofweek[daywk]+finish.strftime(' %d '+monthstr[monthNo]+' %Y %H-%M-%S'))
+print(dayofweek[daywk]+finish.strftime(' %d '+monthstr[monthNo]+' '+str(yearthai)+' %H-%M-%S'))

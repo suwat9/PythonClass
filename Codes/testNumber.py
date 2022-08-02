dbbd80cb55cf3c79483fee8dcfaa66f1e5c758f0
Suwat <suwat.tac@mail.pbru.ac.th>
2022-08-02 09:48:12 +0700

    Create testNumber.py

---
+++ Codes/testNumber.py
@@ -0,0 +1,20 @@
+number = 7
+digit1 = 0xA3
+digit2 = 0o17
+digit3 = 0b101111
+print(number, digit1,digit2,digit3)
+
+print(bin(20))
+print(oct(20))
+print(hex(20))
+
+floating = 7.2858
+f1 = 7/100
+f2 = 7e-06
+
+print(f1,'{:f}'.format(f2))
+
+from datetime import datetime, timedelta
+    
+d = datetime.today() + timedelta(days=2)
+print(d)

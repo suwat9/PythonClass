3fe28fe1b62839255ad4029e8ba82803bc61dfcd
Suwat <suwat.tac@mail.pbru.ac.th>
2022-08-02 11:36:31 +0700

    Update testNumpy.py

--- Codes/testNumpy.py
+++ Codes/testNumpy.py
@@ -3,3 +3,9 @@ import numpy as np
 a = np.arange(1,101).reshape(4, 25)
 
 print(a.shape, a.dtype.name, a.size, a.itemsize)
+
+b = np.array([1,2,3,4,6])
+print(b)
+
+c = np.array([1,2,3.6, 7.9, 15.2])
+print(c, c.dtype.name)

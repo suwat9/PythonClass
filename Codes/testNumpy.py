d9a74a0ba2152bb5742b483c1440828ca3b9fe83
Suwat <suwat.tac@mail.pbru.ac.th>
2022-08-02 11:59:40 +0700

    Update testNumpy.py

--- Codes/testNumpy.py
+++ Codes/testNumpy.py
@@ -9,3 +9,21 @@ print(b)
 
 c = np.array([1,2,3.6, 7.9, 15.2])
 print(c, c.dtype.name)
+
+d = np.zeros((3,3),dtype=np.int16)
+print(d)
+
+e = np.ones((4,4),dtype=np.int16) 
+print(e)
+
+f = np.arange(10,31,5)
+print(f)
+
+g = np.linspace(1,100,50)
+print(g)
+
+seed = np.random.default_rng()
+
+h = seed.random((1,10)) * 17
+h = h.astype(np.int16)
+print(h+1)

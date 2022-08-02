c1ee9fed3be8581207f39c1105d023a6419b1d35
Suwat <suwat.tac@mail.pbru.ac.th>
2022-08-02 12:11:01 +0700

    Create testString.py

---
+++ Codes/testString.py
@@ -0,0 +1,23 @@
+s = 'Suwat'
+
+print(s)
+print(s[0])
+print(s[4])
+
+print(s[::-1])
+print(s[:2])
+print(s[-2:])
+
+lastname = 'tacha'
+
+fullname = s + ' ' + lastname
+
+print(fullname)
+
+split1 = list(fullname)
+print(split1)
+
+print(fullname.split())
+
+for i in split1:
+  print(i)

0d64dad4f354e0924c719ef64ea488657902781a
Suwat <suwat.tac@mail.pbru.ac.th>
2022-08-09 10:42:44 +0700

    Update grade1.py

--- Codes/grade1.py
+++ Codes/grade1.py
@@ -6,4 +6,13 @@ excel_data = pd.read_excel('grade.xlsx')
 # Read the values of the file in the dataframe
 data = pd.DataFrame(excel_data, columns=['subject', 'credit', 'grade'])
 # Print the content
-print("The content of the file is:\n", data)
+#print("The content of the file is:\n", data.subject[0],data.credit[0],data.grade[0])
+
+gradeAverage = 0
+creditSum = 0
+
+for i in range(0,3):
+  gradeAverage = gradeAverage + (data.credit[i]*data.grade[i])
+  creditSum = creditSum + data.credit[i]
+
+print('เกรดเฉลี่ย : '+str(gradeAverage / creditSum))

a74d882423542fa0affdd5a481c0ca953ca8945e
Suwat <suwat.tac@mail.pbru.ac.th>
2022-08-09 10:08:25 +0700

    Update python-IF.py

--- Codes/python-IF.py
+++ Codes/python-IF.py
@@ -55,8 +55,10 @@ print(numbers_to_month(36 % 12))
 
 monthFull = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 monthShort = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+monthThai = ['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม']
 
-monthNo = 11 % 12
+monthNo = 12 % 12
 
 print(monthFull[monthNo])
 print(monthShort[monthNo])
+print(monthThai[monthNo])

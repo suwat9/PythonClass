d19433af5dc942099cde973a0b10ce382c2e81dc
Suwat <suwat.tac@mail.pbru.ac.th>
2022-08-09 11:13:02 +0700

    Create calculateElectric.py

---
+++ Codes/calculateElectric.py
@@ -0,0 +1,19 @@
+# Import pandas
+import pandas as pd
+
+# Load the xlsx file
+excel_data = pd.read_excel('rateElectric.xlsx')
+# Read the values of the file in the dataframe
+data = pd.DataFrame(excel_data, columns=['หน่วยที่ใช้', 'ราคาต่อหน่วย'])
+
+print(data.head())
+
+useUnit = 30
+totalPrice = 0
+nextRate = 1
+while (useUnit > data['หน่วยที่ใช้'][nextRate] ):
+  #print(data['หน่วยที่ใช้'][nextRate])
+  totalPrice = totalPrice +( (data['หน่วยที่ใช้'][nextRate]-data['หน่วยที่ใช้'][nextRate-1]) * data['ราคาต่อหน่วย'][nextRate-1])
+  nextRate = nextRate + 1
+
+print(totalPrice)

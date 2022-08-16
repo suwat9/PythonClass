66cb6ca044acc88a6621e0bb9cf3f6679c932ad4
Suwat <suwat.tac@mail.pbru.ac.th>
2022-08-16 09:34:15 +0700

    Create Lab16-8-2022.py

---
+++ Codes/Lab16-8-2022/Lab16-8-2022.py
@@ -0,0 +1,9 @@
+student = {
+    "id":"654272103",
+    "name":"Sukanya",
+    "lastname":"Mesuk",
+    "year":2565,
+    "school":"Prachuap Khiri Khan"
+}
+
+print(student["name"],student["school"])
